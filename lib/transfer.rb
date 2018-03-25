c48249b473367b3f2e24ced11f49798a3e249105
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-25 22:41:27 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -33,7 +33,11 @@ class Transfer
   end
 
   def reverse_transfer
-
+    if self.status == "complete"
+      self.sender.balance += self.amount
+      self.receiver.balance -= self.amount
+      self.status = "reversed"
+    end
   end
 
 end
