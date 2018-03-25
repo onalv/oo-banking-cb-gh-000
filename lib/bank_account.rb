64d9370294b28d57e68c242346c22353c6c1cef2
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-25 21:52:47 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -6,22 +6,22 @@ class BankAccount
     @balance = 1000
     @status = "open"
   end
-  # 
+  #
   # def balance
   #   @balance
   # end
-  # 
+  #
   # def balance=(balance)
   #   @balance = balance
   # end
-    
+
 
   def deposit(deposit_quantity)
     @balance += deposit_quantity
   end
 
   def display_balance
-    "Your balance is $#{self.balance}."
+    "Your balance is $#{@balance}."
   end
 
   def valid?
