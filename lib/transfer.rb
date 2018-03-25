dc85067555cf5abd8142883d20df477aa6dbd5fc
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-25 22:30:22 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,6 +19,7 @@ class Transfer
         self.receiver.balance += self.amount
         self.status = "complete"
       else
+        self.status = "rejected"
         "Transaction rejected. Please check your account balance."
       end
     end
